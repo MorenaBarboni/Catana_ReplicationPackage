e737146e1a5862b3831f5797df491232490b7e28
morenabarboni <morena.barboni@unicam.it>
2025-01-17 16:53:57 +0100

    Add catana config

--- catana-config.js
+++ catana-config.js
@@ -9,6 +9,6 @@ module.exports = {
     DeployedProxyAddr: "0x241326339ced11ecc7ca07e4aa350234c57f53e5",
     DeployedLogicAddr: "0xcf131548b18d55fb29df2df47b360c41389ebb2b",
     stateVarsBlacklist: ["__gap", "_gap"],
-    INFURA_KEY: "e9d3a8149d144db094236aa0f137547d",
-    ETHERSCAN_KEY: "H3WVD84SYP2G5B8A758DN9XPT2ECIHTNS7"
+    INFURA_KEY: "",
+    ETHERSCAN_KEY: ""
 }
